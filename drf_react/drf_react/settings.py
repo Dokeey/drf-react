780570ef82d963b348d7735a3ba0d8f9b6ae06ea
dokeey <ehdgnv@naver.com>
2020-06-06 16:32:43 +0900

    Add: 챕터2 공부(Queryset의 정렬/범위, debug 도구, 모델관계 필드, migrations 파일관리)

--- drf_react/drf_react/settings.py
+++ drf_react/drf_react/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third apps
+    'django_extensions',
+    'debug_toolbar',
+    # locals apps
+    'accounts',
     'blog1',
     'instagram',
 ]
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'drf_react.urls'
@@ -71,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'drf_react.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -81,7 +84,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -101,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -115,7 +116,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
@@ -124,3 +124,11 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1',
+    # ...
+]
+
+AUTH_USER_MODEL = 'auth.User'
--- drf_react/instagram/admin.py
+++ drf_react/instagram/admin.py
@@ -1,20 +1,31 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .models import Post
+from .models import Post, Comment, Tag
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-  list_display = ['id', 'photo_tag', 'message', 'message_length', 'is_public', 'created_at']
-  list_display_links = ['id', 'message']
-  search_fields = ['message']
-  list_filter = ['is_public', 'created_at']
+    list_display = ['id', 'photo_tag', 'message', 'message_length', 'is_public', 'created_at']
+    list_display_links = ['id', 'message']
+    search_fields = ['message']
+    list_filter = ['is_public', 'created_at']
 
-  # def message_length(self, post):
-  #     return len(post.message)
-  # message_length.short_description = '메세지 글자수'
+    # def message_length(self, post):
+    #     return len(post.message)
+    # message_length.short_description = '메세지 글자수'
 
-  def photo_tag(self, post):
-    if post.photo:
-      return mark_safe(f'<img src="{post.photo.url}" style="width:72px" />')
-    return None
+    def photo_tag(self, post):
+        if post.photo:
+            return mark_safe(f'<img src="{post.photo.url}" style="width:72px" />')
+        return None
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
