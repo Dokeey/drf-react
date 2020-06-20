f13f33733e748200b7b917ac8bdcf5cc30a3a7fa
dokeey <ehdgnv@naver.com>
2020-06-20 20:06:57 +0900

    Add: 챕터 3 공부(뷰의 호출과 응답, URL 정규표현식 적용, FBV/CBV 맛보기)

--- drf_react/instagram/urls.py
+++ drf_react/instagram/urls.py
@@ -1,7 +1,25 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 
 from . import views
 
+app_name = 'instagram'  # URL Reverse에서 namespace 역할
+
+
+class YearConverter:
+    regex = r"20\d{2}"
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return "%04d" % value
+
+
+register_converter(YearConverter, 'year')
+
 urlpatterns = [
     path('', views.post_list),
+    path('<int:pk>/', views.post_detail),
+    # re_path(r'archives/(?P<year>20\d{2})', views.archives_year),
+    path('archives/<year:year>/', views.archives_year),
 ]
--- drf_react/instagram/views.py
+++ drf_react/instagram/views.py
@@ -1,15 +1,34 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
+post_list = ListView.as_view(model=Post)
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q', '')
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q', '')
+#
+#     if q:
+#         qs = qs.filter(message__icontains=q)
+#
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list': qs,
+#         'q': q
+#     })
 
-    if q:
-        qs = qs.filter(message__icontains=q)
 
-    return render(request, 'instagram/post_list.html', {
-        'post_list': qs,
-        'q': q
-    })
+# python hint를 쓰는 추세
+# def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+#     post = get_object_or_404(Post, pk=pk)
+#     # post = Post.objects.get(pk=pk)
+#     return render(request, 'instagram/post_detail.html', {
+#         'post': post,
+#     })
+
+post_detail = DetailView.as_view(model=Post)
+
+
+def archives_year(request, year):
+    return HttpResponse(f"{year}년 archives")
