5c0bd16626dac102002ac3aa6a43712a6fa27d40
dokeey <ehdgnv@naver.com>
2020-07-19 22:10:52 +0900

    Add: 챕터7(프로젝트 생성, Bootstrap4 레이아웃, 커스텀 유저 및 회원가입, SendGrid 메일 설정)

--- chapter4(2).md
+++
@@ -1,4 +0,0 @@
-# DRF-React Study
-
-## Chapter 6. 웹 프론트엔드 기초 및 장고 static
-
---
+++ chapter7-8/nospa_lib/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hood/hood/workspace/DRF-React/chapter7-8/nospa_lib/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
